19f7edf55801ab352c7b7adb8dd07b641dc34e92
viveksundar5 <134808541+viveksundar5@users.noreply.github.com>
2023-09-14 18:58:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ mv change_log_entry.txt change.log
 git config user.email "actions@github.com"
 git config user.name "GitHub Actions"
 git add change.log
-git commit -m "Update change.log with release 1.0.0 changes"
+git commit -m "Update change.log with release 1.0.1 changes"
 git push
