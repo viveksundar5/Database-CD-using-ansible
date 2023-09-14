d941b957219ae6f225ac5df63b42919112063d35
viveksundar5 <134808541+viveksundar5@users.noreply.github.com>
2023-09-14 19:01:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 DEPLOY_SQL_CONTENT=$(cat deploy.sql)
-CHANGE_LOG_ENTRY="\/**** Release : 1.0.0 change START on $(date +\%d\ %b\ \%Y\ :\ %r) ****/
+CHANGE_LOG_ENTRY="\/**** Release : 1.0.1 change START on $(date +\%d\ %b\ \%Y\ :\ %r) ****/
 ${DEPLOY_SQL_CONTENT}
-\/**** Release : 1.0.0 change END ****/"
+\/**** Release : 1.0.1 change END ****/"
 echo "$CHANGE_LOG_ENTRY" > change_log_entry.txt
 cat change.log >> change_log_entry.txt
 mv change_log_entry.txt change.log
